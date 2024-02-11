95b06a4dfb3c17032acbcff1e73da473efab5086
whung <whung@ucsb.edu>
2024-02-11 14:52:41 -0800

    Added PUT api call for ArticlesController and corresponding tests

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -82,4 +82,25 @@ public class ArticlesController extends ApiController {
 
         return articles;
     }
+
+    @Operation(summary= "Update a single article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public Articles updateArticles(
+            @Parameter(name="id") @RequestParam Long id,
+            @RequestBody @Valid Articles incoming) {
+
+        Articles articles = articlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
+
+        articles.setTitle(incoming.getTitle());
+        articles.setEmail(incoming.getEmail());
+        articles.setUrl(incoming.getUrl());
+        articles.setExplanation(incoming.getExplanation());
+        articles.setDateAdded(incoming.getDateAdded());
+
+        articlesRepository.save(articles);
+
+        return articles;
+    }
 }
