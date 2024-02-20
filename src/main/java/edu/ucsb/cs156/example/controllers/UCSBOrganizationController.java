3baa42df2bd09edd1ff4beb6d3e7434855f3bb20
viditagrawal <40546427+viditagrawal@users.noreply.github.com>
2024-02-19 23:19:35 -0800

    change

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.repository.query.Param;
 import javax.validation.Valid;
+
 @Tag(name = "UCSBOrganization")
 @RequestMapping("/api/ucsborganizations")
 @RestController
